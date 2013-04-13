db491294f4378ce7e08b48a1522eacc799d01ade
klobej <josh@alchemy50.com>
2013-04-13 16:23:37 -0400

    adding login flow item two

--- Hackathon/LoginViewController.h
+++ Hackathon/LoginViewController.h
@@ -16,13 +16,28 @@
     UITextField *passwordTextField;
     
     UIView *loginFirstView;
+    
+    UIView *loginSecondView;
+    UIImageView *selectedUserImageView;
+    
+    UIImagePickerController *photoImagePicker;
 }
 
+-(IBAction)createPlayerButtonHit;
+-(IBAction)playUsingFacebookButtonHit;
+
 -(IBAction)signupButtonHit;
+
+-(IBAction)photoButtonHit;
+
 @property (nonatomic, retain) IBOutlet UITextField *usernameTextField;
 @property (nonatomic, retain) IBOutlet UITextField *passwordTextField;
 
 @property (nonatomic, retain) IBOutlet UIView *loginFirstView;
 
+@property (nonatomic, retain) IBOutlet UIView *loginSecondView;
+@property (nonatomic, retain) IBOutlet UIImageView *selectedUserImageView;
+
+@property (nonatomic, retain) IBOutlet UIImagePickerController *photoImagePicker;
 @end
 
--- Hackathon/LoginViewController.m
+++ Hackathon/LoginViewController.m
@@ -19,6 +19,13 @@
 @synthesize usernameTextField;
 @synthesize passwordTextField;
 
+@synthesize loginFirstView;
+
+@synthesize loginSecondView;
+@synthesize selectedUserImageView;
+
+@synthesize photoImagePicker;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -31,7 +38,6 @@
 -(IBAction)signupButtonHit
 {
     AppDelegate *del = (AppDelegate *)[UIApplication sharedApplication].delegate;
-    
     [del.homeViewController loginViewComplete];
     
 }
@@ -40,21 +46,58 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    self.view.backgroundColor = [UIColor redColor];
-    
-
     NSLog(@"self.loginFirstView: %@", self.loginFirstView);
     [self.view addSubview:self.loginFirstView];
-    
-//    UIImageView *
-    // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning
+
+-(IBAction)createPlayerButtonHit
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.view addSubview:self.loginSecondView];
+}
+
+-(IBAction)playUsingFacebookButtonHit
+{
+    
+}
+
+-(IBAction)photoButtonHit
+{
+    if ([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera])
+    {
+        self.photoImagePicker = [[UIImagePickerController alloc] init];
+        self.photoImagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        self.photoImagePicker.showsCameraControls = YES;
+        self.photoImagePicker.delegate = self;
+        CGRect frame = photoImagePicker.view.frame;
+        frame.origin.y += 30;
+        frame.size.height -= 30;
+        self.photoImagePicker.view.frame = frame;
+        [self.view addSubview:self.photoImagePicker.view];
+        
+        self.photoImagePicker.view.transform = CGAffineTransformMakeRotation(-3.141592/2);
+        self.photoImagePicker.view.frame = CGRectMake(580,-5,580,310);
+        
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:.35];
+        self.photoImagePicker.view.frame = CGRectMake(0,self.photoImagePicker.view.frame.origin.y,self.photoImagePicker.view.frame.size.width,self.photoImagePicker.view.frame.size.height);
+        [UIView commitAnimations];
+        
+        
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSLog(@"didFinishPickingMediaWithInfo: %@", info);
+    
+    self.selectedUserImageView.image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:.35];
+    self.photoImagePicker.view.frame = CGRectMake(580,00,self.photoImagePicker.view.frame.size.width,self.photoImagePicker.view.frame.size.height);
+    [UIView commitAnimations];
+    
 }
 
 @end
